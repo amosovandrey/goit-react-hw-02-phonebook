8c105744af21422a942333d2edc7c5bdbc43a390
amosovandrey <rivercity.dp@gmail.com>
2023-07-26 12:26:04 +0300

    step 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,13 @@ import { nanoid } from 'nanoid';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
     name: '',
     number: '',
   };
@@ -36,10 +42,21 @@ class App extends Component {
   };
 
   reset = () => {
-    this.setState({ name: '', number: '' });
+    this.setState({ name: '', number: '', filter: '' });
   };
+
+  changeFilter = evt => {
+    this.setState({ filter: evt.currentTarget.value });
+  };
+
   render() {
     const { name, contacts } = this.state;
+    const normalizedFilter = this.state.filter.toLowerCase();
+
+    const filteredContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
     return (
       <div className={css.wrapper}>
         <div className={css.container}>
@@ -54,7 +71,7 @@ class App extends Component {
                 id={name}
                 type="text"
                 name="name"
-                pattern="^[a-zA-Zа-яА-Я]+(([ '-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                 required
               />
@@ -67,7 +84,7 @@ class App extends Component {
                 onChange={this.handleInputChange}
                 id={name}
                 name="number"
-                pattern="\+?\d{1,4}?[-.\s]?\(?.{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
                 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                 required
               />
@@ -79,8 +96,16 @@ class App extends Component {
         </div>
         <div className={css.container}>
           <h2 className={css.title}>Contacts</h2>
+          <label className={css.label} htmlFor="">
+            Find contacts by name
+            <input
+              type="text"
+              className={css.input}
+              onChange={this.changeFilter}
+            />
+          </label>
           <ul className={css.list}>
-            {contacts.map(contact => (
+            {filteredContacts.map(contact => (
               <li className={css.list__item} key={contact.id}>
                 <span>{contact.name}:</span>
                 <span className={css.list__item__number}>{contact.number}</span>
